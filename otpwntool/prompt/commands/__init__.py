b915a22d9da39fef83e4641b4748ca9d7f24a057
stuxve <121454920+stuxve@users.noreply.github.com>
2026-01-01 16:40:59 +0100

    Delete otpwntool directory

--- otpwntool/prompt/commands/__init__.py
+++
@@ -1,4 +0,0 @@
-from .commands import COMMANDS
-from .nested_commands import NESTED_COMMANDS
-from .handler import CommandHandler
-from .completer import CommandCompleter
--- otpwntool/prompt/commands/nested_commands.py
+++
@@ -1,3 +0,0 @@
-NESTED_COMMANDS = {
-    
-}
